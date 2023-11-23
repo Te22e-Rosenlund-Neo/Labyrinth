a59ce1800acae988a8abfaaaebf810f6e0f1e4a2
Neo Rosenlund <Neo.rosenlund@elev.ga.ntig.se>
2023-11-23 10:59:47 +0100

    made arrays  and rooms

--- Labyrinth-Game/Program.cs
+++ Labyrinth-Game/Program.cs
@@ -1,10 +1,41 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
-Rooms[] AllRooms;
-Items[] AllItems;
+List<Items> AllItems;
+Rooms[,] RoomArea = new Rooms[10,10];
+
+Rooms Start = new(true, true, true, true, 0);    //4 openings
+Rooms Corridor = new(true, false, true, false, 0); // 2 openings
+Rooms[] Clearance0 = {Start, Corridor};
+
+Rooms Kitchen = new(true, true, false, false, 1); //  2 openings
+Rooms Bedroom = new(true, false, false, false, 1); // 1 openings
+Rooms BathingHall = new(true, false, true, false, 1); // 2 openings
+Rooms Library = new(true, true, true, false, 1); // 3 openings
+Rooms[] Clearance1 = {Kitchen, Bedroom, BathingHall, Library};
+
+Rooms TortureChamber = new(true, false, false, false, 1); // 1 openings
+Rooms MainHall = new(true ,true, true, true, 1); // 4 openings
+Rooms BathRoom = new(true, false, true, false, 1); // 2 openings
+Rooms[] Clearance2 = {TortureChamber, MainHall, BathRoom};
+
+Rooms Garden = new(true, false, false, false, 1); // 1 openings EXIT
+
+
+
+RoomArea[5,5] = Start;
+
+
+bool RoomsExist(int x, int y){
+if(x == 0 && x>10 && y == 0 && y>10){
+    return true;
+}
+return false;
+}
 
 
 
 
 
+Console.ReadKey();
 
--- Labyrinth-Game/Rooms.cs
+++ Labyrinth-Game/Rooms.cs
@@ -1,17 +1,50 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
 class Rooms{
 
-string RoomsName;
+ string RoomsName;
 bool escapable;
+int RoomClearanceLvl;
+//up left down right
+public bool[] DoorPossible = {false, false, false, false};
+
+
+bool Collapsed = false;
+
+
+public Rooms(bool Door1, bool Door2, bool Door3, bool Door4, int RoomClearanceLvl){
+
+    bool[] TempDoorPos = {Door1, Door2, Door3, Door4};
+    DoorPossible = TempDoorPos;
+    this.RoomClearanceLvl = RoomClearanceLvl;
+}
 
 
 
 
-    Rooms(String Name, bool escapable){
-        RoomsName = Name;
-        this.escapable = escapable;
-        
 
-    }
+
+
+
+
+
+
+
+public void RoomDescription(){
+
+    Console.WriteLine($"You Entered {RoomsName}");
+    Console.WriteLine(@"			Top Rooms
+
+
+
+	Left room			Right Room
+
+		
+			Bottom room");
+
+}
 
 
 
